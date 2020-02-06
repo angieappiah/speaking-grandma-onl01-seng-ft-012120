4f78564a6e2c9b480dc206ccfd43aa20f705b9a6
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-06 05:08:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,4 @@
-
-def speak_to_grandma 
-  if phrase == "I LOVE YOU GRANDMA!"
-    elsif phrase ==phrase. upcase
-    return " NO, NOT SINCE 1938"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
-  end
+def speak_to_grandma
+  
+  
+end
